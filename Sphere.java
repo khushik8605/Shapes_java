c62e44eb8381f89ffe82d8d00a6ff630ba0bcb27
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-07 23:45:27 +0530

    area sphere

--- Sphere.java
+++ Sphere.java
@@ -3,6 +3,12 @@ class Sphere extends Shape {
     public Sphere(double radius) {
         super(radius, 0, 0, 0);
     }
+
+    public double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
+
+     
   
 }
 
