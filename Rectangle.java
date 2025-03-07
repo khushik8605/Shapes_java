4a0d0727678cc48a488e0521be345bddf65ecfc4
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-07 23:25:45 +0530

    area calculator (rectangle)

--- Rectangle.java
+++ Rectangle.java
@@ -2,6 +2,9 @@ class Rectangle extends Shape {
     public Rectangle(double length, double width) {
         super(length, width, 0, 4);
     }
+    public double calculateArea() {
+        return dim_one * dim_two;
+    }
 
     
 }
