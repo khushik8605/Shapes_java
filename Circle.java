8d9b864a88fcfb7a209c5374aa20a6ba745d7576
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-07 23:19:54 +0530

    add area calculation fnc for circle

--- Circle.java
+++ Circle.java
@@ -3,6 +3,12 @@ class Circle extends Shape {
     public Circle(double radius) {
         super(radius, 0, 0, 0);
     }
+    
+    public double calculateArea() {
+        return Math.PI * dim_one * dim_one;
+    }
+
+    
 
 }
 
