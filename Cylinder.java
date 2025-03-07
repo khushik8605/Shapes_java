96c0f5308f6af2846ab39c05d86b7d2edad48ee1
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-07 23:22:34 +0530

    area calculator

--- Cylinder.java
+++ Cylinder.java
@@ -3,6 +3,11 @@ class Cylinder extends Shape {
     public Cylinder(double radius, double height) {
         super(radius, height, 0, 0);
     }
+    public double calculateArea() {
+        return 2 * Math.PI * dim_one * (dim_one + dim_two);
+    }
+
+    
 
     
 }
