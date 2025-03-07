f23660f6133d29571d42d651460a05b532d87c31
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-07 23:45:46 +0530

    volume sphere

--- Sphere.java
+++ Sphere.java
@@ -8,7 +8,13 @@ class Sphere extends Shape {
         return 4 * Math.PI * dim_one * dim_one;
     }
 
-     
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
+    }
+
+        public double calculatePerimeter() {
+        return 0; // No perimeter for a sphere
+    }
   
 }
 
